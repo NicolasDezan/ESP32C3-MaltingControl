9cd72b8b0334c36f169654a6482d5a59ec322638
Nicolas Dezan <nicolinugfx@gmail.com>
2025-02-27 19:29:50 -0300

    [bluetooth_config] Reorganização do código para melhor legibilidade

--- project/lib/utils/bluetooth_config.py
+++ project/lib/utils/bluetooth_config.py
@@ -1,16 +1,15 @@
 import aioble
 import bluetooth
 
-BT_SERVICE_UUID = bluetooth.UUID('12345678-1234-1234-1234-1234567890ab')  # Serviço principal
-BT_WRITE_UUID = bluetooth.UUID('12345678-1234-1234-1234-1234567890ac') 
-BT_READ_UUID = bluetooth.UUID('12345678-1234-1234-1234-1234567890ad')
+NAME = "ESP32-BLE"
 
 ADV_INTERVAL_MS = 250_000
 
-NAME = "ESP32-BLE"
-
+BT_SERVICE_UUID = bluetooth.UUID('12345678-1234-1234-1234-1234567890ab')
 bt_service = aioble.Service(BT_SERVICE_UUID)
 
+BT_WRITE_UUID = bluetooth.UUID('12345678-1234-1234-1234-1234567890ac') 
 write_characteristic = aioble.Characteristic(bt_service, BT_WRITE_UUID, read=True, notify=True)
 
+BT_READ_UUID = bluetooth.UUID('12345678-1234-1234-1234-1234567890ad')
 read_characteristic = aioble.Characteristic(bt_service, BT_READ_UUID, read=True, write=True, notify=True, capture=True)
