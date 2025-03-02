37941cc8bf6806306133a44415271cf56a78890e
Nicolas Dezan <nicolinugfx@gmail.com>
2025-03-02 12:32:43 -0300

    [CONSTS] Criação de consts_group.py para centralizar as constantes de COMANDOS (Leitura e Escrita) e de PARÂMETROS (Valor mínimo e Fator de Multiplicação)

---
+++ project/data/consts_groups.py
@@ -0,0 +1,52 @@
+class ReadList:
+    CHANGE_PARAMETERS = 0
+    SEND_PARAMETERS = 1
+
+class WriteList:
+    SEND_PARAMETERS = 1
+    HEARTBEAT = 255
+
+
+class ParametersCorrections:
+    class Steeping:
+        class SubmergedTime:
+            MIN_VALUE = 1.0
+            MULT_TEN = 1.0
+
+        class WaterVolume:
+            MIN_VALUE = 200.0
+            MULT_TEN = 10.0
+
+        class RestTime:
+            MIN_VALUE = 0.0
+            MULT_TEN = 0.1
+
+        class Cycles:
+            MIN_VALUE = 1.0
+            MULT_TEN = 1.0
+
+    class Germination:
+        class RotationLevel:
+            MIN_VALUE = 0.0
+            MULT_TEN = 1.0
+
+        class TotalTime:
+            MIN_VALUE = 24.0
+            MULT_TEN = 1.0
+
+        class WaterVolume:
+            MIN_VALUE = 100.0
+            MULT_TEN = 1.0
+
+        class WaterAddition:
+            MIN_VALUE = 10.0
+            MULT_TEN = 1.0
+
+    class Kilning:
+        class Temperature:
+            MIN_VALUE = 40.0
+            MULT_TEN = 1.0
+
+        class Time:
+            MIN_VALUE = 1.0
+            MULT_TEN = 0.1
