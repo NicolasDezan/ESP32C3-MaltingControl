f1ccf9e6b0893564eaaece2fde172479715dac87
Nicolas Dezan <nicolinugfx@gmail.com>
2025-03-02 12:34:32 -0300

    [Parameters] Comunicação de envio e recebimento de parâmetros funcionando 100%

--- project/lib/utils/task_handler.py
+++ project/lib/utils/task_handler.py
@@ -1,26 +1,19 @@
 import asyncio
 import utils.data_converter as dt
-from tasks.parameters_tasks import change_parameters_task 
+from tasks.parameters_tasks import change_parameters_task, send_parameters_task
+from data.consts_groups import WriteList,ReadList
 
 async def task_handler(data):
     try:
         array_byte = dt.decode_to_array_byte(data)
 
         
-        if   array_byte[0] == 0:
+        if   array_byte[0] == ReadList.CHANGE_PARAMETERS:
             asyncio.create_task(change_parameters_task(array_byte))
-        elif array_byte[0] == 1:
-            asyncio.create_tast() # ...
+
+        elif array_byte[0] == ReadList.SEND_PARAMETERS:
+            asyncio.create_task(send_parameters_task(array_byte))
         
-
-
-
-
-
-
-
-
-
 
     except Exception as e:
         print("ERROR in task_handler(): ", e)
--- project/tasks/parameters_tasks.py
+++ project/tasks/parameters_tasks.py
@@ -1,21 +1,83 @@
 import data.init_data as parameter
+from data.consts_groups import WriteList, ParametersCorrections
+import asyncio
 
 async def change_parameters_task(array_byte):
-    print("_change_parameters_task() was CALLED: ", array_byte)
+    print("change_parameters_task() was CALLED: ", array_byte)
 
-    parameter.steeping_submerged_time = array_byte[1]
-    parameter.steeping_water_volume = array_byte[2]
-    parameter.steeping_rest_time = array_byte[3]
-    parameter.steeping_cycles = array_byte[4]
+    # Steeping
+    parameter.steeping_submerged_time = (
+        array_byte[1] * ParametersCorrections.Steeping.SubmergedTime.MULT_TEN
+        + ParametersCorrections.Steeping.SubmergedTime.MIN_VALUE
+    )
+    parameter.steeping_water_volume = (
+        array_byte[2] * ParametersCorrections.Steeping.WaterVolume.MULT_TEN
+        + ParametersCorrections.Steeping.WaterVolume.MIN_VALUE
+    )
+    parameter.steeping_rest_time = (
+        array_byte[3] * ParametersCorrections.Steeping.RestTime.MULT_TEN
+        + ParametersCorrections.Steeping.RestTime.MIN_VALUE
+    )
+    parameter.steeping_cycles = (
+        array_byte[4] * ParametersCorrections.Steeping.Cycles.MULT_TEN
+        + ParametersCorrections.Steeping.Cycles.MIN_VALUE
+    )
 
-    parameter.germination_rotation_level = array_byte[5]
-    parameter.germination_total_time = array_byte[6]
-    parameter.germination_water_volume = array_byte[7]
-    parameter.germination_water_addition = array_byte[8]
+    # Germination
+    parameter.germination_rotation_level = (
+        array_byte[5] * ParametersCorrections.Germination.RotationLevel.MULT_TEN
+        + ParametersCorrections.Germination.RotationLevel.MIN_VALUE
+    )
+    parameter.germination_total_time = (
+        array_byte[6] * ParametersCorrections.Germination.TotalTime.MULT_TEN
+        + ParametersCorrections.Germination.TotalTime.MIN_VALUE
+    )
+    parameter.germination_water_volume = (
+        array_byte[7] * ParametersCorrections.Germination.WaterVolume.MULT_TEN
+        + ParametersCorrections.Germination.WaterVolume.MIN_VALUE
+    )
+    parameter.germination_water_addition = (
+        array_byte[8] * ParametersCorrections.Germination.WaterAddition.MULT_TEN
+        + ParametersCorrections.Germination.WaterAddition.MIN_VALUE
+    )
 
-    parameter.kilning_temperature = array_byte[9]
-    parameter.kilning_time = array_byte[10]
+    # Kilning
+    parameter.kilning_temperature = (
+        array_byte[9] * ParametersCorrections.Kilning.Temperature.MULT_TEN
+        + ParametersCorrections.Kilning.Temperature.MIN_VALUE
+    )
+    parameter.kilning_time = (
+        array_byte[10] * ParametersCorrections.Kilning.Time.MULT_TEN
+        + ParametersCorrections.Kilning.Time.MIN_VALUE
+    )
 
     parameter.rewrite_data()
 
     parameter.print_current_data()
+
+
+async def send_parameters_task(array_byte):
+    from tasks.main_tasks import write_task
+    print("send_parameters_task() was CALLED: ", array_byte)
+
+    asyncio.run(
+        write_task([
+            WriteList.SEND_PARAMETERS,
+
+            # Steeping
+            int((parameter.steeping_submerged_time - ParametersCorrections.Steeping.SubmergedTime.MIN_VALUE) / ParametersCorrections.Steeping.SubmergedTime.MULT_TEN),
+            int((parameter.steeping_water_volume - ParametersCorrections.Steeping.WaterVolume.MIN_VALUE) / ParametersCorrections.Steeping.WaterVolume.MULT_TEN),
+            int((parameter.steeping_rest_time - ParametersCorrections.Steeping.RestTime.MIN_VALUE) / ParametersCorrections.Steeping.RestTime.MULT_TEN),
+            int((parameter.steeping_cycles - ParametersCorrections.Steeping.Cycles.MIN_VALUE) / ParametersCorrections.Steeping.Cycles.MULT_TEN),
+
+            # Germination
+            int((parameter.germination_rotation_level - ParametersCorrections.Germination.RotationLevel.MIN_VALUE) / ParametersCorrections.Germination.RotationLevel.MULT_TEN),
+            int((parameter.germination_total_time - ParametersCorrections.Germination.TotalTime.MIN_VALUE) / ParametersCorrections.Germination.TotalTime.MULT_TEN),
+            int((parameter.germination_water_volume - ParametersCorrections.Germination.WaterVolume.MIN_VALUE) / ParametersCorrections.Germination.WaterVolume.MULT_TEN),
+            int((parameter.germination_water_addition - ParametersCorrections.Germination.WaterAddition.MIN_VALUE) / ParametersCorrections.Germination.WaterAddition.MULT_TEN),
+
+            # Kilning
+            int((parameter.kilning_temperature - ParametersCorrections.Kilning.Temperature.MIN_VALUE) / ParametersCorrections.Kilning.Temperature.MULT_TEN),
+            int((parameter.kilning_time - ParametersCorrections.Kilning.Time.MIN_VALUE) / ParametersCorrections.Kilning.Time.MULT_TEN)
+        ])
+    )
