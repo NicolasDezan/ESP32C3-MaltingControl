d641a557b33b59f64c8fb928975e27585259ca32
Nicolas Dezan <nicolinugfx@gmail.com>
2025-03-26 23:47:27 -0300

    chore: adiciona comentários para indicar a unidade de medida

--- project/data/default_data.py
+++ project/data/default_data.py
@@ -1,16 +1,16 @@
 default_data = {
     "was_in_process": False,
     
-    "steeping_submerged_time": 1,
-    "steeping_water_volume": 200,
-    "steeping_rest_time": 25.5,
+    "steeping_submerged_time": 1,  # horas
+    "steeping_water_volume": 200, # mL
+    "steeping_rest_time": 25.5, # horas
     "steeping_cycles": 1,
 
     "germination_rotation_level": 0,
-    "germination_total_time": 24,
-    "germination_water_volume": 100,
-    "germination_water_addition": 10,
+    "germination_total_time": 24, # horas
+    "germination_water_volume": 100, # mL
+    "germination_water_addition": 10, # mL
 
-    "kilning_temperature": 40,
-    "kilning_time": 1.0
+    "kilning_temperature": 40, # graus_celsius
+    "kilning_time": 1.0 # horas
 }
