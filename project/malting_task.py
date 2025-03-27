f5bb8462281b5a5c99a13779117e479956bddad0
Nicolas Dezan <nicolinugfx@gmail.com>
2025-03-26 23:48:31 -0300

    feat: inicia a implementação dos malting stages

--- project/malting_task.py
+++ project/malting_task.py
@@ -1,6 +1,7 @@
 import asyncio
+from lib.utils.uptime import Uptime; uptime = Uptime()
 
-# Estado global simplificado
+# Estado global
 malting_control = {
     "active": False,
     "current_stage": None,
@@ -26,7 +27,6 @@ async def malting_task():
 
 
 async def execute_malting_stages():
-    """Orquestração das etapas de malteação"""
     await steeping_stage()
 
     if malting_control["abort_flag"].is_set():
@@ -41,24 +41,60 @@ async def execute_malting_stages():
 
 
 async def steeping_stage():
-    """Etapa de hidratação dos grãos"""
+    import data.init_data as setpoint
     malting_control["current_stage"] = "steeping"
-    print("STEEPING Started")
-    
+    print("=== STEEPING STARTED ===")
+
     try:
-        start = 0
+        cycle = 0
         
-        while start < 3:
-            start = start + 1
+        while cycle < setpoint.steeping_cycles:
+            cycle += 1
+            print(f"\n[Cycle {cycle}/{setpoint.steeping_cycles}]")
+            
+            # 1. Encher água - Correlação de tempo de valvula ligada e `setpoint.steeping_water_volume`
+            print("Filling water...")
             if malting_control["abort_flag"].is_set():
+                print("! ABORTED DURING FILLING !")
                 return
-            print("STEEPING Running")
-            await asyncio.sleep(1)
+            
+            # 2. Esperar tempo submerso
+            print(f"Submerged for {setpoint.steeping_submerged_time}h")
+            init_time = uptime.minutes()
+            while (uptime.minutes() < (setpoint.steeping_submerged_time*60 + init_time)):
+                
+                if malting_control["abort_flag"].is_set():
+                    print("! ABORTED DURING SUBMERGED TIME !")
+                    return
+                print(f"Submerged: {(uptime.minutes() - init_time)}/{setpoint.steeping_submerged_time*60}")
+
+                await asyncio.sleep(1)
+            
+            # 3. Remover água - abrir valvula 
+            print("Draining water...")
+            if malting_control["abort_flag"].is_set():
+                print("! ABORTED DURING DRAINING !")
+                return
+            
+            # 4. Esperar tempo de descanso
+            print(f"Resting for {setpoint.steeping_rest_time}h")
+            init_time = uptime.minutes()
+            while (uptime.minutes() < (setpoint.steeping_rest_time*60 + init_time)):
+                
+                if malting_control["abort_flag"].is_set():
+                    print("! ABORTED DURING REST TIME !")
+                    return
+                print(f"Resting: {(uptime.minutes() - init_time)}/{setpoint.steeping_rest_time*60}")
+
+                await asyncio.sleep(1)
         
-        print("STEEPING Completed")
+        print("\n=== STEEPING COMPLETED ===")
     
     finally:
-        pass
+        print("...")
+
+
+
 
 
 async def germination_stage():
@@ -73,6 +109,8 @@ async def germination_stage():
             start = start + 1
             if malting_control["abort_flag"].is_set():
                 return
+            
+            # CONTROLE AQUI!
             print("GERMINATION Running")
             await asyncio.sleep(1)
         
@@ -94,6 +132,8 @@ async def kilning_stage():
             start = start + 1
             if malting_control["abort_flag"].is_set():
                 return
+            
+            # CONTROLE AQUI!
             print("KILNING Running")
             await asyncio.sleep(1)
         
