439bffca0e90474d59a8d04cb0f9544b20a16779
Nicolas Dezan <nicolinugfx@gmail.com>
2025-03-28 11:51:18 -0300

    refactor: separa do código os algoritmos das etapas

--- project/tasks/malting_task.py
+++ project/tasks/malting_task.py
@@ -1,5 +1,5 @@
 import asyncio
-from lib.utils.uptime import Uptime; uptime = Uptime()
+
 
 # Estado global
 malting_control = {
@@ -10,12 +10,11 @@ malting_control = {
 
 
 async def malting_task():
-    """Tarefa principal simplificada de malteação"""
+    # Loop principal que verifica se deve iniciar a malteação
     while True:
         # Aguarda o comando de início
         while not malting_control["active"]:
-            await asyncio.sleep(0.1)
-
+            await asyncio.sleep(1)
         try:
             # Loop principal do processo
             await execute_malting_stages()
@@ -26,7 +25,13 @@ async def malting_task():
             await finish_process()
 
 
+from tasks.malting_stages.steeping import steeping_stage
+from tasks.malting_stages.germination import germination_stage
+from tasks.malting_stages.kilning import kilning_stage
 async def execute_malting_stages():
+    print("=== MALTING STARTED ===")
+    print("===       ...       ===")
+
     await steeping_stage()
 
     if malting_control["abort_flag"].is_set():
@@ -40,109 +45,6 @@ async def execute_malting_stages():
     await kilning_stage()
 
 
-async def steeping_stage():
-    import data.init_data as setpoint
-    malting_control["current_stage"] = "steeping"
-    print("=== STEEPING STARTED ===")
-
-    try:
-        cycle = 0
-        
-        while cycle < setpoint.steeping_cycles:
-            cycle += 1
-            print(f"\n[Cycle {cycle}/{setpoint.steeping_cycles}]")
-            
-            # 1. Encher água - Correlação de tempo de valvula ligada e `setpoint.steeping_water_volume`
-            print("Filling water...")
-            if malting_control["abort_flag"].is_set():
-                print("! ABORTED DURING FILLING !")
-                return
-            
-            # 2. Esperar tempo submerso
-            print(f"Submerged for {setpoint.steeping_submerged_time}h")
-            init_time = uptime.minutes()
-            while (uptime.minutes() < (setpoint.steeping_submerged_time*60 + init_time)):
-                
-                if malting_control["abort_flag"].is_set():
-                    print("! ABORTED DURING SUBMERGED TIME !")
-                    return
-                print(f"Submerged: {(uptime.minutes() - init_time)}/{setpoint.steeping_submerged_time*60}")
-
-                await asyncio.sleep(1)
-            
-            # 3. Remover água - abrir valvula 
-            print("Draining water...")
-            if malting_control["abort_flag"].is_set():
-                print("! ABORTED DURING DRAINING !")
-                return
-            
-            # 4. Esperar tempo de descanso
-            print(f"Resting for {setpoint.steeping_rest_time}h")
-            init_time = uptime.minutes()
-            while (uptime.minutes() < (setpoint.steeping_rest_time*60 + init_time)):
-                
-                if malting_control["abort_flag"].is_set():
-                    print("! ABORTED DURING REST TIME !")
-                    return
-                print(f"Resting: {(uptime.minutes() - init_time)}/{setpoint.steeping_rest_time*60}")
-
-                await asyncio.sleep(1)
-        
-        print("\n=== STEEPING COMPLETED ===")
-    
-    finally:
-        print("...")
-
-
-
-
-
-async def germination_stage():
-    """Etapa de germinação dos grãos"""
-    malting_control["current_stage"] = "germination"
-    print("GERMINATION Started")
-    
-    try:
-        start = 0
-
-        while start < 5:
-            start = start + 1
-            if malting_control["abort_flag"].is_set():
-                return
-            
-            # CONTROLE AQUI!
-            print("GERMINATION Running")
-            await asyncio.sleep(1)
-        
-        print("GERMINATION Completed")
-    
-    finally:
-        pass
-
-
-async def kilning_stage():
-    """Etapa de secagem/kilning"""
-    malting_control["current_stage"] = "kilning"
-    print("KILNING Started")
-    
-    try:
-        start = 0
-
-        while start < 2:
-            start = start + 1
-            if malting_control["abort_flag"].is_set():
-                return
-            
-            # CONTROLE AQUI!
-            print("KILNING Running")
-            await asyncio.sleep(1)
-        
-        print("KILNING Completed")
-    
-    finally:
-        pass
-
-
 async def finish_process():
     """Reset básico do sistema"""
     print("FINISH PROCESS")
@@ -152,13 +54,10 @@ async def finish_process():
 
 
 """
-            Como usar:
+Iniciar processo:
+    malting_control["active"] = True
+    malting_control["abort_flag"].clear()
 
-                Iniciar processo (em outra tarefa):
-                    malting_control["active"] = True
-                    malting_control["abort_flag"].clear()
-
-                Parar processo (a qualquer momento):
-                    malting_control["abort_flag"].set()"
-
+Parar processo:
+    malting_control["abort_flag"].set()"
 """
