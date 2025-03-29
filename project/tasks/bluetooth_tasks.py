dd6d5d441e219a021bb49d560b3b86e46e5e95bf
Nicolas Dezan <nicolinugfx@gmail.com>
2025-03-29 14:07:38 -0300

    refactor: adiciona 2 dígitos decimais no envio do monitoramento de consumo de RAM

--- project/tasks/bluetooth_tasks.py
+++ project/tasks/bluetooth_tasks.py
@@ -37,8 +37,15 @@ async def read_task():
 # Tarefa para envio de cores a cada 2 segundos
 async def send_heartbeat_task():
     while True:
-        _memory_usage = int(memory_usage())
-        message = [WriteList.HEARTBEAT,_memory_usage]
+        _memory_usage_A = int(memory_usage())
+        _memory_usage_B = int((memory_usage()*100) % 100)
+        
+        message = [
+            WriteList.HEARTBEAT,
+            _memory_usage_A,
+            _memory_usage_B
+        ]
+        
         bt.write_characteristic.write(bytes(message), send_update=True)
         await asyncio.sleep(2)
 
