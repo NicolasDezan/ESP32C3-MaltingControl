8cf9d73fc3f3c761a017fed310ecd527d423084f
Nicolas Dezan <nicolinugfx@gmail.com>
2025-04-12 19:07:12 -0300

    feat: adiciona a função setup para inicialização dos sensores e ativa a tarefa sensors_task()

--- project/main.py
+++ project/main.py
@@ -1,7 +1,24 @@
 import asyncio
 from tasks.bluetooth_tasks import peripheral_task, send_heartbeat_task, read_task
 from tasks.malting_task import malting_task
+from tasks.sensors_task import sensors_task
+from lib.sensors.sensor_checker import init_sensors, check_sensor_status
+from lib.sensors.sensor_reader import get_sensor_readings
 import data.init_data
+
+def setup():
+    print("Inicializando sensores...")
+    init_sensors()
+    status = check_sensor_status()
+    print("Status dos sensores:", status)
+
+    if not any(status.values()):
+        print("Nenhum sensor disponível. Verifique a conexão.")
+    else:
+        if not status["AHT20"]:
+            print("AHT20 não está funcionando corretamente.")
+        if not status["ENS160"]:
+            print("ENS160 não está funcionando corretamente.")
 
 async def main():
     # Cria três tarefas assíncronas que serão executadas simultâneamente:
@@ -18,8 +35,20 @@ async def main():
     # 4. Tarefa para gerenciamento do processo de malteação
     malting = asyncio.create_task(malting_task())
 
+    # 5. Tarefa para gerenciamento dos sensores
+    sensors = asyncio.create_task(sensors_task())
+
     # Cria o loop principal juntando as tarefas
-    await asyncio.gather(send_heartbeat, peripheral, read_data, malting)
+    await asyncio.gather(
+        send_heartbeat, 
+        peripheral, 
+        read_data, 
+        malting,
+        sensors
+    )
+
+# Faz a inicialização e verificações necessárias
+setup()
 
 # Inicia o loop principal
 asyncio.run(main())
